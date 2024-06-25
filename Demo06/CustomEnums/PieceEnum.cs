be46274b07b475bd0b61150b64a70158ef406250
studentdot0500 <studentdot0500@de.techni.dom>
2024-06-25 16:39:17 +0200

    Demo 06 - Enum

---
+++ Demo06/CustomEnums/PieceEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06.CustomEnums
+{
+    public enum PieceEnum
+    {
+        Pion = 1,
+        Tour = 5,
+        Fou = 6,
+        Cavalier = 7,
+        Reine = 11,
+        Roi = 15
+    }
+}
---
+++ Demo06/Program.cs
@@ -0,0 +1,34 @@
+﻿using Demo06.CustomEnums;
+
+// Déclarer une variable du type de l'enum
+PieceEnum p1 = PieceEnum.Reine;
+
+// Parcourir les valeurs de l'enum
+Console.WriteLine("Valeur 1");
+foreach (int val in Enum.GetValues(typeof(PieceEnum)) )
+{
+    Console.WriteLine($" - {val} / {(PieceEnum)val}");
+}
+Console.WriteLine();
+
+Console.WriteLine("Valeur 2");
+foreach (int val in Enum.GetValues<PieceEnum>() )
+{
+    Console.WriteLine($" - {val} / {(PieceEnum)val}");
+}
+Console.WriteLine();
+
+// Parcourir les noms de l'enum
+Console.WriteLine("Nom 1");
+foreach (string name in Enum.GetNames(typeof(PieceEnum)) )
+{
+    Console.WriteLine($" - {name}");
+}
+Console.WriteLine();
+
+Console.WriteLine("Nom 2");
+foreach (string name in Enum.GetNames<PieceEnum>() )
+{
+    Console.WriteLine($" - {name}");
+}
+Console.WriteLine();
